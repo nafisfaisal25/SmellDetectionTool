92d146cb5c77b34a74aa310abc3689c51a95fcc1
nafisfaisal25 <nafisfaisal25@gmail.com>
2018-12-11 11:03:41 +0600

    add git clone functionality

---
+++ src/iit/du/GitClonehandler.java
@@ -0,0 +1,36 @@
+package iit.du;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GitClonehandler
+ */
+@WebServlet("/git_clone")
+public class GitClonehandler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GitClonehandler() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
